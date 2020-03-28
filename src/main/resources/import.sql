7fcfa5b2f603c715b677b7918eee29662ceb41f4
LOLO\Pc <rafael_v56@hotmail.com>
2020-03-28 20:56:00 +0100

    Se mejoro al usuario
    Ahora se puede ver la edad y su fecha de nacimiento
    
    Se actualizo en la BBDD los atributos, se actualizo tambien el index y la vista
    del usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 /*Insertarmos unos clientes de prueba*/
-INSERT INTO usuarios(id,nombre,peso,talla)VALUES (1,'rafa',60,160);
-INSERT INTO usuarios(id,nombre,peso,talla)VALUES (2,'ana',50,160);
+INSERT INTO usuarios(id,nombre,peso,talla,dia,mes,anno,hora,minuto)VALUES (1,'jero',60,160,4,10,2004,2,0);
+INSERT INTO usuarios(id,nombre,peso,talla,dia,mes,anno,hora,minuto)VALUES (2,'rafa',60,160,2,10,1984,16,0);
+
